6408b5852f9b73f73e0914c77ef2b58e8f1114a4
Adarsh10-coder <btwadarsh1018@gmail.com>
2025-08-17 19:22:40 +0530

    hooks done

---
+++ 3_counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let counter = 5
+  let [counter, setCounter] = useState(15)
+  // this(hooks) help to change counter variable in whole web-page where counter is displaying
+
+  const addValue = () =>{
+    //counter = counter+1;
+    setCounter(counter+1)
+  }
+  const removevalue = () =>{
+    setCounter(counter-1)
+  }
+
+  return (
+    <>
+     <h1>chai aur reactLogo</h1>
+     <h2>Counter value: {counter}</h2>
+
+     <button onClick={addValue}>Add value</button>
+     <br />
+     <button onClick={removevalue}>Remove value</button>
+    </>
+  )
+}
+
+export default App
