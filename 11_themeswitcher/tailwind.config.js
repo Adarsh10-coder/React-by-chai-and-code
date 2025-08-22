05b477232d68f76de6fdda622ed67ea9cb81286c
Adarsh10-coder <btwadarsh1018@gmail.com>
2025-08-23 00:03:02 +0530

    theme switcher done

---
+++ 11_themeswitcher/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",           // for Vite
+    "./src/**/*.{js,ts,jsx,tsx}", // for React files
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
